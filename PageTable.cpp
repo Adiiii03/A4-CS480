5abffee6f48e7bbf207b21c68a14dd23f284cd1b
Kaylie Pham <161658889+kayliepham@users.noreply.github.com>
2025-04-16 02:40:37 -0700

    Update PageTable.cpp
    added create_pagetable

--- PageTable.cpp
+++ PageTable.cpp
@@ -31,7 +31,55 @@ Map::Map() {
     dirty = false;              // nothing wrriten 
 }
 
-// constructor for the Pagetable
-PageTable::PageTable(int bitPerLevel[], int levelCount){
-    this->levelCount = levelCount;
-};
+PageTable* create_pagetable(int bitsPerLevel[]) {
+
+  PageTable* newPageTable = new PageTable;  // create new page table
+
+  newPageTable->levelCount = sizeof(bitsPerLevel) / sizeof(bitsPerLevel[0]); // store number of levels
+
+  // store count of bits for offset; offset = 32 - (number of bits for each level)
+  newPageTable->numOfBitsOffset = 32;   // assuming the address has 32 bits, initialize to 32 and decrement from there
+  for (int i = 0; i < bitsPerLevel[0]; i++) {
+    newPageTable->numOfBitsOffset -= bitsPerLevel[i];
+  }
+
+  // create array of bitmasks corresponding to level i
+  newPageTable->bitMaskAry = new int[newPageTable->levelCount];
+  for (int i = 0; i < newPageTable->levelCount; i++) {
+    newPageTable->bitMaskAry[i];
+  }
+
+  // create array of bits to shift for getting level i page index
+  newPageTable->shiftAry = new int[newPageTable->levelCount];
+  int shiftAryAmt = 32;   // assuming the address has 32 bits, initialize to 32 and decrement from there
+  for (int i = 0; i < newPageTable->levelCount; i++) {
+    shiftAryAmt -= bitsPerLevel[i];
+    newPageTable->shiftAry[i] = shiftAryAmt;
+  }
+
+  // create array of # of next level entries for level i
+       // for each level, the entry count is 2^(number of bits for level i)
+  newPageTable->entryCount = new int[newPageTable->levelCount];
+  int base = 2;    // base is 2 because two binary values: 0 and 1
+  int exponent = 0;
+  for (int i = 0; i < newPageTable->levelCount; i++) {
+    exponent = bitsPerLevel[i];  // exponent = number of bits for level i
+    newPageTable->entryCount[i] = std::pow(base, exponent);
+  }
+
+  // create root level with depth 0, entry count for the first level (so index 0), page table pointer
+  newPageTable->rootLevel = create_level(0, newPageTable->entryCount[0], newPageTable);
+
+  // additional variable for summary
+  exponent = newPageTable->numOfBitsOffset;
+  int pageSize = std::pow(base, exponent);               // bytes per page = 2^(offset)
+
+  // all initialized to 0 because no addresses have been processed, replaced, mapped, allocated
+  newPageTable->numOfPageReplaced = 0;      // number of page replacement
+  newPageTable->pageTableHits = 0;          // number of times a virtual page was mapped
+  newPageTable->numOfAddress = 0;           // number of addresses processed
+  newPageTable->numOfFramesAllocated = 0;   // number of frames allocated
+  newPageTable->pgTableEntries = 0;         // total number of page table entries acrosss all levels
+
+  return newPageTable;
+}
