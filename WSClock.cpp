c862ab2a30abd60f491e19ab6c21590aa45ee3c2
Aditya Bhagat <141161589+Adiiii03@users.noreply.github.com>
2025-04-18 03:42:51 -0700

    Update WSClock.cpp

--- WSClock.cpp
+++ WSClock.cpp
@@ -11,7 +11,48 @@ WSClockEntry* create_WSClock_entry(unsigned int vpn, int frameNum, int lastAcces
     return WSClock_entry;
 }
 
-void page_replacement(WSClockEntry* entries, int clock_hand_position){
+void page_replacement(WSClockEntry** entries, int clock_hand_position, int ageThreshold, int currentTime, unsigned int newVpn, PageTable* PageTable){
 
-    
+
+    bool validFrame = false;             // initializing validFrame to false
+
+
+    while(!validFrame) {
+        WSClockEntry* entry = entries[clock_hand_position];
+
+        int ageOfLastAccessConsideredRecent = currentTime - (entry->lastUsedTime);
+
+        //checking if the age of the page is greater than threshold
+        if (ageOfLastAccessConsideredRecent > ageThreshold){
+
+            // checking if the page is clean or not
+            if (entry->dirty == false){
+                // invalidating the current vpn
+                unsigned int evictedVPN = entry->vpn;               // evictedVPN: setting with vpn of the current evicted page
+                Map* victimMap = findVpn2PfnMapping(PageTable, evictedVPN);
+
+                if (victimMap != nullptr) {
+                    victimMap->valid = false;
+                }
+
+                // updating WSClock entry with new vpn information
+                entry->vpn = newVpn;
+                entry->lastUsedTime = currentTime;
+                entry->dirty = false;
+
+                // inserting the new vpn mapping to pagetable
+                insertVpn2PfnMapping(PageTable, newVpn, entry->frameNum);
+
+                // franeNum for the victim page
+                int victimFrame = entry->frameNum;
+
+                // advancing clock_hand_position
+                clock_hand_position = clock_hand_position + 1;
+        }
+    }
+        else{
+            // clearing the dirty flag
+            entry->dirty = false;
+        }
+    }
 }
