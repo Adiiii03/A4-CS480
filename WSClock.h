cbd48654a210fbe7028e625207234c232b57c72b
Aditya Bhagat <141161589+Adiiii03@users.noreply.github.com>
2025-04-17 23:07:49 -0700

    Create WSClock.h

---
+++ WSClock.h
@@ -0,0 +1,23 @@
+// === WSClock.h ===
+#ifndef WSCLOCK_H
+#define WSCLOCK_H
+
+#include <vector>
+#include <cstdint>
+
+struct WSClockEntry{
+    unsigned int vpn;       // virtual page number (full)
+    int frameNum;           // physical frame number
+    int lastUsedTime;       // time this page was last accessed
+    bool dirty;             // true if page has been written to
+};
+
+WSClockEntry create_WSClock_entry(unsigned int vpn, int frameNum, int lastAccessTime, bool dirty); 
+
+//void page_replacement(WSClockEntry* entries, int clock_hand_position);
+
+// perform WSClock page replacement algorithm
+// returns frame number to use and updates oldVPN with evicted VPN
+int page_replacement(std::vector<WSClockEntry>& clock, int& clockHand, int ageThreshold, int currentTime, unsigned int& oldVPN);
+
+#endif
