4ee2b15981e464140786d4c08e4598d2c96d1c57
Kaylie Pham <161658889+kayliepham@users.noreply.github.com>
2025-04-17 19:22:00 -0700

    Update pagingwithwsclock.cpp

--- pagingwithwsclock.cpp
+++ pagingwithwsclock.cpp
@@ -10,21 +10,28 @@
 
 #include "vaddr_tracereader.h"
 #include "page_table.h"
+#include "log_helpers.h"
 
 
 int main(int argc, char* argv[]) {
 
-    int numMemAccesses;     // only prpcess first N memory accesses, default is all
+    int numOfAddresses = 0;         // count of addresses processed
+    int frameNum = 0;           // stores physical frame number, which starts at 0 (and will be sequentially incremented)
+    p2AddrTr mtrace;    // structure is typedefed in vaddr_tracereader
+    unsigned int vAddr;    // unsigned 32-bit integer type
+    bool cont_addresses = true;    // if true, continue retrieving addresses
+    
+    int maxNumMemAccesses = -1;     // only process first N memory accesses, default is all
     int numPhysicalFrames = 999999;     // number of available physical frames, default is 999999
     int ageRecentLastAccess = 10;     // age of last access considered recent for page replacement, default is 10
-    const char* logMode = "summary";       // log mode, specifying what to printed, default is "summary"
+    const char* logMode = "summary";       // log mode, specifying what to be printed, default is "summary"
 
     int opt;
     while ((opt = getopt(argc, argv, "n:f:a:l")) != -1) {
         switch (opt) {
             case 'n':
-                numMemAccesses = atoi(optarg);
-                if (numMemAccesses < 1) {
+                maxNumMemAccesses = atoi(optarg);
+                if (maxNumMemAccesses < 1) {
                   std::cerr << "Number of memory accesses must be a number, greater than 0" << std::endl;
                   return 1;    // exit prgram, error occurred
                   }
@@ -98,14 +105,33 @@ int main(int argc, char* argv[]) {
          return 1; // exit program, error occurred
        }
     }
+    
+    PageTable* page_table = create_pagetable(bitsPerLevel);
+    
+    while (NextAddress(mtrace_file, &mtrace) && cont_addresses) {     // while next address exists...
+        vAddr = mtrace.addr;        // ...store next address
+        insertVpn2PfnMapping(page_table, vAddr, frameNum);  // insert VPN
+        numOfAddresses++;   // increment number of addresses processed
 
-    p2AddrTr mtrace;    // structure is typedefed in vaddr_tracereader
-    uint32_t vAddr;    // unsigned 32-bit integer type
-    if (NextAddress(mtrace_file, &mtrace)) {    // if next address exists...
-      vAddr = mtrace.addr;    // ...store next address
+        // if max number of memory accesses is specified (in command line)
+                  // and if max number of memory accesses is reached, then exit while loop
+        if (maxNumMemAccesses && (numOfAddresses >= maxNumMemAccesses)) {
+                cont_addresses = false;
+            }
+        }
+
+    // print logs given the log mode from command line
+    if (logMode == "bitmasks") {    // print bit masks for each level, starting with lowest tree level
+        log_bitmasks(page_table->levelCount, page_table->bitMaskAry);
+    }
+    else if (logMode == "summary") {
+        log_summary(page_table->pageSize, page_table->numOfPageReplaced, page_table->pageTableHits,
+            page_table->numOfAddresses, page_table->numOfFramesAllocated, page_table->pgTableEntries);
     }
 
-    PageTable* page_table = create_pagetable(bitsPerLevel);
+    // close files before exiting
+    fclose(mtrace_file);
+    fclose(readswrites_file);
 
-    return 0;
+    return 0; // exit program, as it successfully complete
 }
